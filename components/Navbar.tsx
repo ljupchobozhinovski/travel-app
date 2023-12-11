13f02b792c8bfc0a517b0242ec8920eb37bcf374
Ljupcho Bozhinovski <ljupchobozhinovski@gmail.com>
2023-12-11 13:01:53 +0100

    add hero page

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,9 +2,11 @@ import Link from "next/link"
 import Image from "next/image"
 import { NAV_LINKS } from "@/constants"
 import Button from "./Button"
+import { useState } from "react"
 
 const Navbar = () => {
-    return (
+
+       return (
         <nav className="flexBetween max-container padding-container relative z-30 py-5">
             <Link href="/">
                 <Image src="/hilink-logo.svg" alt={"logo"} width={74} height={29} />
