05e7828fa6697bc1963c051db1842ed5d95c1172
Ljupcho Bozhinovski <ljupchobozhinovski@gmail.com>
2023-12-18 16:29:26 +0100

    add get app section

--- components/Button.tsx
+++ components/Button.tsx
@@ -5,15 +5,16 @@ type ButtonProps = {
     title: string;
     icon?: string;
     variant: string
+    full?: boolean
 }
 
-const Button = ({ title, icon, type, variant }: ButtonProps) => {
+const Button = ({ title, icon, type, variant, full }: ButtonProps) => {
     return (
         <button
-            className={`flexCenter gap-3 rounded-full border ${variant} `}
+            className={`flexCenter gap-3 rounded-full border ${variant} ${full && "w-full"}`}
             type={type}>
             {icon && <Image src={icon} alt={title} width={24} height={24} />}
-            <label className='bold-16 whitespace-nowrap' htmlFor="">{title}</label>
+            <label className='bold-16 whitespace-nowrap cursor-pointer' htmlFor="">{title}</label>
         </button>
     )
 }
